8807b5a52723623121ca3d11e667ccba417f7deb
Heeme Beeme <heemebeeme@gmail.com>
2026-01-22 14:19:04 -0400

    Level 2 + 3 level changers + starting from any level

--- Assets/Systems/Managers/LevelManager.cs
+++ Assets/Systems/Managers/LevelManager.cs
@@ -9,6 +9,11 @@ public class LevelManager : MonoBehaviour
     public GameObject To;
     public GameObject CurrentLevel;
 
+    public void Start()
+    {
+        CurrentLevel = GameObject.FindGameObjectWithTag("Level");
+    }
+
     public void LevelChange()
     {
         CurrentLevel.SetActive(false);
